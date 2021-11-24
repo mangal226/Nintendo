cf482c114f5256fb433e6a0d3836cd10f68b97d9
Mannonn <manonj@hotmail.fr>
2021-11-24 12:35:22 +0100

    ajout c1 et c2

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,9 +1,12 @@
 package test;
 
+import metier.Client;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Client c1 = new Client("Dupont","Martin");
+		Client c2 = new Client("Dupond","Alice");
 
 	}
 
